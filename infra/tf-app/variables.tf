c799433d7b8ffb8ea06453db480231266393479a
Todd Oneil <onei0240@algonquinlive.com>
2026-07-27 17:07:13 -0400

    added initial tf-app terraform files and configurations

---
+++ infra/tf-app/variables.tf
@@ -0,0 +1,31 @@
+# backend references
+variable "state_resource_group" {
+  description = "resource group"
+  type        = string
+  default     = "onei-sar-githubactions-rg"
+}
+
+variable "state_storage" {
+  description = "state location"
+  type        = string
+  default     = "oneisargithubactions"
+}
+
+variable "state_cont" {
+  description = "state container"
+  type        = string
+  default     = "tfstate"
+}
+
+# tf-app variables
+variable "region" {
+  description = "region for all resources"
+  type        = string
+  default     = "canadacentral"
+}
+
+variable "resource_group" {
+  description = "resource group"
+  type        = string
+  default     = "onei-sar-a12-rg"
+}
