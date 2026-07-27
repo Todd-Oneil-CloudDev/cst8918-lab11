4cad5d8bf20a275cefff2cf5790650bd3d255885
saraMir26 <mirz0014@algonquinlive.com>
2026-07-27 18:58:42 -0400

    Removed extra variables from variable file and modified the infra-ci-cd file

--- infra/tf-app/variables.tf
+++ infra/tf-app/variables.tf
@@ -6,41 +6,9 @@ variable "subscription_id" {
   type        = string
 }
 
-
-/*Since these values are comming from prod.backend.hcl we dont need to define them here.  
-
-variable "state_resource_group" {
-  description = "resource group"
-  type        = string
-  default     = "onei-sar-githubactions-rg"
-}
-
-variable "state_storage" {
-  description = "state location"
-  type        = string
-  default     = "oneisargithubactions"
-}
-
-variable "state_cont" {
-  description = "state container"
-  type        = string
-  default     = "tfstate"
-}
-
-*/
-
 # tf-app variables
 variable "region" {
   description = "region for all resources"
   type        = string
   default     = "canadacentral"
 }
-
-/* variable is not being used
-
-variable "resource_group" {
-  description = "resource group"
-  type        = string
-  default     = "onei-sar-a12-rg"
-}
-*/
