63da381a5f56fac22c6e26c950d7d626ad08194f
Todd Oneil <onei0240@algonquinlive.com>
2026-07-27 21:13:29 -0400

    fixed formatting

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -4,15 +4,15 @@ resource "azurerm_resource_group" "app" {
 }
 
 resource "azurerm_virtual_network" "app_network" {
-  name = "onei-sar-a12-network"
+  name                = "onei-sar-a12-network"
   resource_group_name = azurerm_resource_group.app.name
-  location = azurerm_resource_group.app.location
-  address_space = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.app.location
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "app-subnet" {
-  name = "onei-sar-a12-subnet"
-  resource_group_name = azurerm_resource_group.app.name
+  name                 = "onei-sar-a12-subnet"
+  resource_group_name  = azurerm_resource_group.app.name
   virtual_network_name = azurerm_virtual_network.app_network.name
-  address_prefixes = [ "10.0.0.1/24" ]
+  address_prefixes     = ["10.0.0.1/24"]
 }
