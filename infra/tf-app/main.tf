9aaf9d26da5a31b8b37f535fcc52fd055ecb65a5
Todd Oneil <onei0240@algonquinlive.com>
2026-07-27 21:15:18 -0400

    fixed naming convention

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "app_network" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "app-subnet" {
+resource "azurerm_subnet" "app_subnet" {
   name                 = "onei-sar-a12-subnet"
   resource_group_name  = azurerm_resource_group.app.name
   virtual_network_name = azurerm_virtual_network.app_network.name
